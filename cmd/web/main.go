273f9d562dd531cdee131e1c275def91609b05cc
dominic-wassef <domwassef@gmail.com>
2023-04-15 21:48:46 -0400

    session middleware, usage in main func & routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,10 +10,13 @@ import (
 	"terminus/internal/driver"
 	"terminus/internal/models"
 	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const version = "1.0.0"
-const cssVersion = "1"
+
+var session *scs.SessionManager
 
 type config struct {
 	port int
@@ -35,6 +38,7 @@ type application struct {
 	templateCache map[string]*template.Template
 	version       string
 	DB            models.DBModel
+	Session       *scs.SessionManager
 }
 
 func (app *application) serve() error {
@@ -75,6 +79,10 @@ func main() {
 
 	defer conn.Close()
 
+	// set up session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+
 	tc := make(map[string]*template.Template)
 
 	app := &application{
@@ -84,6 +92,7 @@ func main() {
 		templateCache: tc,
 		version:       version,
 		DB:            models.DBModel{DB: conn},
+		Session:       session,
 	}
 
 	err = app.serve()
