5d274a272f6a82edc9297285ffc94dbd77edaa8e
dominic-wassef <domwassef@gmail.com>
2023-04-21 19:11:36 -0400

    adding to handler api for subs

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -7,6 +7,7 @@ import (
 	"terminus/internal/cards"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/stripe/stripe-go/v72"
 )
 
 type stripePayload struct {
@@ -14,8 +15,14 @@ type stripePayload struct {
 	Amount        string `json:"amount"`
 	PaymentMethod string `json:"payment_method"`
 	Email         string `json:"email"`
+	CardBrand     string `json:"card_brand"`
+	ExpiryMonth   string `json:"exp_month"`
+	ExpiryYear    string `json:"exp_year"`
 	LastFour      string `json:"last_four"`
 	Plan          string `json:"plan"`
+	ProductID     string `json:"product_id"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
 }
 
 type jsonResponse struct {
@@ -109,8 +116,35 @@ func (app *application) CreateCustomerAndSubscribeToPlan(w http.ResponseWriter, 
 
 	app.infoLog.Println(data.Email, data.LastFour, data.PaymentMethod, data.Plan)
 
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: data.Currency,
+	}
+
 	okay := true
-	msg := ""
+	var subscription *stripe.Subscription
+
+	stripeCustomer, msg, err := card.CreateCustomer(data.PaymentMethod, data.Email)
+	if err != nil {
+		app.errorLog.Println(err)
+		okay = false
+	}
+
+	if okay {
+		subscription, err = card.SubscribeToPlan(stripeCustomer, data.Plan, data.Email, data.LastFour, "")
+		if err != nil {
+			app.errorLog.Println(err)
+			okay = false
+		}
+		app.infoLog.Println("subscription id is:", subscription.ID)
+	}
+
+	// TODO: Create transaction, creating a customer locally, creating a order and saving
+	if okay {
+
+	}
+	// msg := ""
 
 	resp := jsonResponse{
 		OK:      okay,
