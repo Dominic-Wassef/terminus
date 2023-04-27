33b787ea810fadcb0f212940b2549f9afdee092c
dominic-wassef <domwassef@gmail.com>
2023-04-27 03:38:30 -0400

    helper func for writeJSON and invalidCredentials

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,29 +5,51 @@ import (
 	"errors"
 	"io"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
-	// sanity checks
-	maxBytes := 1048576
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-
-	// decoder
-	dec := json.NewDecoder(r.Body)
-	err := dec.Decode(data)
+// writeJSON writes aribtrary data out as JSON
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
-		app.errorLog.Println(err)
 		return err
 	}
 
-	// decode again
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		return errors.New("Body must only have a single JSON value!")
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+
 	return nil
 }
 
+// readJSON reads json from request body into data. We only accept a single json value in the body
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576 // max one megabyte in request body
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	// we only allow one entry in the json file
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only have a single JSON value")
+	}
+
+	return nil
+}
+
+// badRequest sends a JSON response with status http.StatusBadRequest, describing the error
 func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err error) error {
 	var payload struct {
 		Error   bool   `json:"error"`
@@ -43,6 +65,37 @@ func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err e
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
 	w.Write(out)
 	return nil
+}
+
+func (app *application) invalidCredentials(w http.ResponseWriter) error {
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	payload.Error = true
+	payload.Message = "invalid authentication credentials"
+
+	err := app.writeJSON(w, http.StatusUnauthorized, payload)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *application) passwordMatches(hash, password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
